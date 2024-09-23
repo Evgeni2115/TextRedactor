ece6ccf3637dea71efae97a859fcb20abff8769b
Evgeni R <reshetnikov_evgeni21@mail.ru>
2024-09-23 14:03:45 +0300

    version2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main extends TextProcessor {
+public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         String inputStr = console.nextLine();
@@ -10,10 +10,49 @@ public class Main extends TextProcessor {
     }
 
     public static String textModifier(String inputStr) {
-        inputStr = removeExtraSpaces(inputStr);
-        inputStr = swapCharactersAroundDash(inputStr);
-        inputStr = replacePlusWithExclamation(inputStr);
-        inputStr = removeDigitsAndCalculateSum(inputStr);
-        return inputStr;
+        StringBuilder result = new StringBuilder();
+        boolean lastWasSpace = false;
+        for (char c : inputStr.toCharArray()) {
+            if (c == ' ') {
+                if (!lastWasSpace) {
+                    result.append(c);
+                    lastWasSpace = true;
+                }
+            } else {
+                result.append(c);
+                lastWasSpace = false;
+            }
+        }
+
+        StringBuilder swappedResult = new StringBuilder();
+        for (int i = 0; i < result.length(); i++) {
+            char current = result.charAt(i);
+
+            if (current == '-' && i > 0 && i < result.length() - 1) {
+                char left = result.charAt(i - 1);
+                char right = result.charAt(i + 1);
+                swappedResult.setCharAt(swappedResult.length() - 1, right);
+                swappedResult.append(left);
+                i++;
+            } else {
+                swappedResult.append(current);
+            }
+        }
+
+        String replacedResult = swappedResult.toString().replace('+', '!');
+
+        StringBuilder finalResult = new StringBuilder();
+        int digitSum = 0;
+        for (char c : replacedResult.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digitSum += Character.getNumericValue(c);
+            } else {
+                finalResult.append(c);
+            }
+        }
+        if (digitSum > 0) {
+            finalResult.append(" ").append(digitSum);
+        }
+        return finalResult.toString();
     }
 }
